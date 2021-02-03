07b598fb3c487858cb7e6995a520b6c0484b3498
Ville Tuomaala <ville.tuomaala@almamedia.fi>
2021-02-03 22:05:46 +0200

    5.7* blogilistan frontend, step7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,46 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+import Button from './Button'
+
+const Blog = ({ blog }) => {
+  const [viewMode, setViewMode] = useState('minified')
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const handleViewModeChange = () => {
+    setViewMode(viewMode === 'minified' ? 'extended' : 'minified')
+  }
+
+  const getBlogDescription = (blog) => {
+    if (viewMode === 'minified') {
+      return (
+        <div>
+          <strong>{blog.title}</strong> ::: {blog.author} <Button type="button" handler={handleViewModeChange} buttonName={viewMode === 'minified' ? 'view' : 'hide'} />
+        </div>
+      )
+    } else if (viewMode === 'extended') {
+      return (
+        <div>
+          <strong>{blog.title}</strong> ::: {blog.author} <Button type="button" handler={handleViewModeChange} buttonName={viewMode === 'minified' ? 'view' : 'hide'} /><br />
+          {blog.url}<br />
+          likes {blog.likes}<br />
+          {blog.user ? blog.user.name : 'not available'}<br />
+          {blog.id}
+        </div>
+      )
+    }
+  }
+
+  return (
+    <div style={blogStyle}>
+      {getBlogDescription(blog)}
+    </div>
+  )
+}
 
 export default Blog
